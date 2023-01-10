e8809cb7ed5fc8cbb9ccbf983b8233f198695111
Muhammad Hunzalah Shakeel <92629025+MuhammadHunzalahShakeel@users.noreply.github.com>
2023-01-10 15:28:26 +0500

    make some changes in responsive
    make some changes in responsive

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -4,6 +4,7 @@ import LinkedinIcon from "../../assets/LinkedinIcon.svg"
 import BehanceIcon from "../../assets/BehanceIcon.svg"
 import InstagramIcon from "../../assets/InstagramIcon.svg"
 import FacebookIcon from "../../assets/FacebookIcon.svg"
+import EmailIcon from "../../assets/EmailIcon.svg"
 
 
 const Connect = () => {
@@ -19,7 +20,8 @@ const Connect = () => {
           <li className='connect_icon' id='connect_linkedin'><a href="https://www.linkedin.com/company/protodev/" rel='noopener' target='_blank'><img className='connect_icon' src={LinkedinIcon} alt="" /></a></li>
           <li className='connect_icon' id='connect_behance'><a href="https://www.behance.net/protodev" rel='noopener' target='_blank'><img className='connect_icon' src={BehanceIcon} alt="" /></a></li>
           <li className='connect_icon' id='connect_instagram'><a href="https://instagram.com/protodev.co?igshid=Yzg5MTU1MDY=" rel='noopener' target='_blank'><img className='connect_icon' src={InstagramIcon} alt="" /></a></li>
-          <li className='connect_icon' id='connect_email'><a href="https://www.facebook.com/profile.php?id=100088868936092" rel='noopener' target='_blank'><img className='connect_icon' src={FacebookIcon} alt="" /></a></li>
+          <li className='connect_icon' id='connect_facebook'><a href="https://www.facebook.com/profile.php?id=100088868936092" rel='noopener' target='_blank'><img className='connect_icon' src={FacebookIcon} alt="" /></a></li>
+          <li className='connect_icon' id='connect_email'><a href="https://www.facebook.com/profile.php?id=100088868936092" rel='noopener' target='_blank'><img className='connect_icon' src={EmailIcon} alt="" /></a></li>
         </ul>
         
       </div>
