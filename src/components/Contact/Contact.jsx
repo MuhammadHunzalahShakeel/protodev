b23eb65ef45282bee7599d78abf8d950120cee70
Muhammad Hunzalah Shakeel <92629025+MuhammadHunzalahShakeel@users.noreply.github.com>
2023-01-11 06:05:22 +0500

    changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,18 +5,20 @@ import ContactImage from "../../assets/ContactImage.svg"
 import SubmitButton from "../../assets/SubmitButton.svg"
 
 const Contact = () => {
-  const form = useRef();
+    const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+    const sendEmail = (e) => {
+      e.preventDefault();
 
-    emailjs.sendForm('service_o8tplwq','template_cruxbrw', form.current,'62o-Tx0ft0e1vcTrF')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
+      emailjs.sendForm('service_4nfnhem', 'template_5bfulk7', form.current, '62o-Tx0ft0e1vcTrF')
+        .then((result) => {
+            console.log(result.text);
+            console.log("messagesent")
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+   
 
   return (
    
@@ -34,16 +36,18 @@ const Contact = () => {
           <p className='contact_paragraph'>Finding the best approach for your needs. Then, transform it into a delightfull interface.</p>
           <div className="contact_inputfields">
             <form ref={form} onSubmit={sendEmail}>
-            <input type="text" name='firstname' class="contact_firstname"  placeholder="First Name" required></input>
-            <input type="text" name='lastname'  class="contact_lastname" placeholder="Last Name"></input>
-            <input type="email" name='email'  class="contact_emailaddress" placeholder="Email Address" required></input>
-            <input type="text" name='budget'  class="contact_budget" placeholder="Budget"></input>
-            <input type="text" name='subject'  class="contact_subject" placeholder="Subject" required></input>
-            <input type="text" name='message'  class="contact_message" placeholder="Message" required></input>
-            {/* <button type="submit" value="Send" >
-              submit
-            </button> */}
-            <a type="submit" href="">
+              <input type="text" name='user_firstname' class="contact_firstname"  placeholder="First Name" required></input>
+              <input type="text" name='user_lastname'  class="contact_lastname" placeholder="Last Name"></input>
+              <input type="email" name='user_email'  class="contact_emailaddress" placeholder="Email Address" required></input>
+              <input type="text" name='user_budget'  class="contact_budget" placeholder="Budget"></input>
+              <input type="text" name='user_subject'  class="contact_subject" placeholder="Subject" required></input>
+              <input type="text" name='message'  class="contact_message" placeholder="Message" required></input>
+              {/* <input type="submit" value="Send"/> */}
+              {/* <img className='contact_submitbutton' alt=''  src={SubmitButton} ></img> */}
+              {/* </input> */}
+              {/* <input type="submit" value="Send"><img className='contact_submitbutton' alt=''  src={SubmitButton} ></img></input> */}
+
+            <a type="submit" href="" >
             <img className='contact_submitbutton' alt=''  src={SubmitButton} ></img>
             </a>
             </form>
