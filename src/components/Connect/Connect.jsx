f084a23254e05d03c2ecfad31ab153aecfaf072f
Muhammad Hunzalah Shakeel <92629025+MuhammadHunzalahShakeel@users.noreply.github.com>
2023-01-14 21:20:50 +0500

    changes

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -14,7 +14,7 @@ const Connect = () => {
           <li className='connect_icon' id='connect_linkedin'><a href="https://www.linkedin.com/company/protodev/" rel='noopener' target='_blank'><img className='connect_icon' src={LinkedinIcon} alt="" /></a></li>
           <li className='connect_icon' id='connect_instagram'><a href="https://instagram.com/protodev.co?igshid=Yzg5MTU1MDY=" rel='noopener' target='_blank'><img className='connect_icon' src={InstagramIcon} alt="" /></a></li>
           <li className='connect_icon' id='connect_facebook'><a href="https://www.facebook.com/profile.php?id=100088868936092" rel='noopener' target='_blank'><img className='connect_icon' src={FacebookIcon} alt="" /></a></li>
-          <li className='connect_icon' id='connect_email'><a href="https://mail.google.com/mail/u/0/#inbox?compose=info@protodev.co" rel='noopener' target='_blank'><img className='connect_icon' src={EmailIcon} alt="" /></a></li>
+          <li className='connect_icon' id='connect_email'><a href="mailto:clients@protodev.co" rel='noopener' target='_blank'><img className='connect_icon' src={EmailIcon} alt="" /></a></li>
         </ul>
       </div>
     </section>
