c4b52c8de92bb210971403099f666bd12da89ecc
Muhammad Hunzalah Shakeel <92629025+MuhammadHunzalahShakeel@users.noreply.github.com>
2023-01-14 22:40:33 +0500

    final changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,10 +13,10 @@ const Footer = () => {
         </div>
         <ul className='footer_nav'>
             <li className="footer_links" id='footer_home'> <a href="#home">Home</a></li>
+            <li className="footer_links" id='footer_about'><a href="#aboutus">About Us</a></li>
             <li className="footer_links" id='footer_services'><a href="#services">Services</a></li>
             <li className="footer_links" id='footer_portfolio'><a href="#protfolio">Protfolio</a></li>
             <li className="footer_links" id='footer_testimonials'><a href="#testimonials">Testimonials</a></li>
-            <li className="footer_links" id='footer_about'><a href="#aboutus">About Us</a></li>
             <li className="footer_links" id='footer_contact'><a href="#contact">Contact Us</a></li>
         </ul>
         <div className="footer_line"></div>
