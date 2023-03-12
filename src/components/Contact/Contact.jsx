eb4474514ad42113f1984b60d260891e5e96f681
Muhammad Hunzalah Shakeel <92629025+MuhammadHunzalahShakeel@users.noreply.github.com>
2023-03-12 06:28:03 +0500

    Revert "v2"

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,69 @@
+import React, { useRef } from 'react';
+import {useState} from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import emailjs from '@emailjs/browser';
+import "./Contact.css"
+import ContactImage from "../../assets/ContactImage.svg"
+import SubmitButton from "../../assets/SubmitButton.svg"
+
+
+
+const Contact = () => {
+  // <script>
+  //   var input = documnet.queryselector("#phonenumber");
+  //   window.intlTelInput(input,{});
+  // </script>
+    const [value, setValue] = useState()
+    const [show, setShow] = useState("flex")
+    const form = useRef();
+    const sendEmail = (e) => {
+      if(show==="flex"){
+          setShow("none");
+          e.preventDefault();
+          e.target.reset();
+          emailjs.sendForm('service_4nfnhem', 'template_5bfulk7', form.current, '62o-Tx0ft0e1vcTrF')
+            .then((result) => {
+                console.log(result.text);
+                toast.success("We receive your message.Our Team will get back to you soon!!",
+                  {theme: "dark"});
+                  setShow("flex");
+              }, (error) => {
+                console.log(error.text);
+                setShow("flex");
+              });
+      }
+        };
+    
+   
+  return (
+    <section id='contact'>
+    <div className="contact_container">
+        <div className="contact_leftcontainer">
+        <img className='contact_image' alt=''  src={ContactImage} ></img>
+        </div>
+        <div className="contact_rightcontainer">
+          <h1 className="contact_firstheading">Let's Discuss</h1>
+          <div className="contact_secondheading">
+              <span className="contact_secondheading1">Your</span>
+              <span className="contact_secondheading2"> Ideas</span>
+          </div>
+          <p className='contact_paragraph'>We love listening your ideas and we make sure that our clients are always satisfied with the services provided. Just tell us what you need and wait for the best results ever.</p>
+          <div className="contact_inputfields">
+            <form ref={form} onSubmit={sendEmail}>
+              <input type="text" name='user_fullname' className="contact_firstname"  placeholder="Full Name" required></input>
+              <input type="email" name='user_email'  className="contact_emailaddress" placeholder="Email Address" required></input>
+              <input type="text" name='user_subject'  class="contact_subject" placeholder="Subject" required></input>
+              <input type="text" name='user_budget'  className="contact_budget" placeholder="Budget" required></input>
+              <input type="text" name='message'  className="contact_message" placeholder="Message" required></input>
+              <button type="submit"> <img className='contact_submitbutton' alt='' style={{display:show}}  src={SubmitButton} ></img></button>
+              <ToastContainer />
+            </form>
+          </div>
+        </div> 
+      </div>
+    </section>
+  )
+}
+
+export default Contact
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,129 @@
+import React,{useState} from 'react'
+import "./Services.css"
+import ServicesNumber1 from "../../assets/ServicesNumber1.svg"
+import ServicesNumber2 from "../../assets/ServicesNumber2.svg"
+import ServicesNumber3 from "../../assets/ServicesNumber3.svg"
+import ServicesNumber4 from "../../assets/ServicesNumber4.svg"
+import ServicesNumber5 from "../../assets/ServicesNumber5.svg"
+import ServicesNumber6 from "../../assets/ServicesNumber6.svg"
+import ServicesArrowButton from "../../assets/ServicesArrowButton.svg"
+
+const Services = () => {
+  const[show1,setShow1]=useState(false);
+  const[show2,setShow2]=useState(false);
+  const[show3,setShow3]=useState(false);
+  const[show4,setShow4]=useState(false);
+  const[show5,setShow5]=useState(false);
+  const[show6,setShow6]=useState(false);
+ 
+  return (
+   
+    <section id='services'>
+       <div className="services_container">
+        <div className="services_heading">What We Do</div>
+        <p className="service_paragraph">Finding the best approach for your needs. 
+        Then, transform it into a delightfull interface, pleasure experience with impactful message.
+        </p>
+        <div className="divider"></div>
+
+        {/* SERVICE NO 01 */}
+        <div className="service1">
+        <div><img className='services_number1' alt='' src={ServicesNumber1} ></img></div>
+        <div className='services1_para'>At Protodev, our UI/UX design services help businesses create user-friendly and visually appealing digital products.</div>
+        <div onClick={()=>setShow1(!show1)}><img id='serviceimage1'  className='service_arrowbutton' alt='' src={ServicesArrowButton} ></img></div>
+        </div>
+        {show1 && <div className='services1_innerpara'>
+        <span className='service_mobile'>At Protodev, our UI/UX design services help businesses create user-friendly and visually appealing digital products.</span>
+        Our team of experts will work with you to understand your business goals and design an intuitive user interface that enhances the user experience. Our services includes:
+          <ul className='services1_innerul'>
+            <li>Website Design</li>
+            <li>App Design</li>
+            <li>Product Design</li>
+            <li>Landing Page Design</li>
+          </ul>
+        </div>
+        
+          }
+        <div className="divider"></div>
+
+        {/* SERVICE NO 02 */}
+        <div className="service1">
+        <div><img className='services_number1' alt='' src={ServicesNumber2} ></img></div>
+        <div className='services1_para'>Our graphic design services at Protodev are tailored to help your business stand out. 
+         </div>
+        <div onClick={()=>setShow2(!show2)}><img className='service_arrowbutton' alt='' src={ServicesArrowButton} ></img></div>
+        </div>
+        {show2 && <div className='services1_innerpara'>We create visually stunning designs that align with your brand and communicate your message effectively. Our services includes: 
+          <ul className='services1_innerul'>
+            <li>Logo Design</li>
+            <li>Brand Identity </li>
+            <li>Product Packaging Design</li>
+            <li>Marketing materials (brochures, flyers, posters, etc.)</li>
+            <li>Product design</li>
+            <li>Infographics</li>
+          </ul>
+        </div>}
+        <div className="divider"></div>
+
+        {/* SERVICE NO 03 */}
+        <div className="service1">
+        <div ><img className='services_number1' alt='' src={ServicesNumber3} ></img></div>
+        <div className='services1_para'>Our website and application development services at Protodev are designed to help businesses create high-performing and scalable digital products.</div>
+        <div onClick={()=>setShow3(!show3)}><img className='service_arrowbutton' alt='' src={ServicesArrowButton} ></img></div>
+        </div>
+        {show3 && <div className='services1_innerpara'> We specialize in creating responsive websites and mobile applications that are visually appealing, user-friendly, and easy to navigate. We specialize into the following domain:
+          <ul className='services1_innerul'>
+            <li>Frontend Development (React Js)</li>
+            <li>Backend Development (Node JS, Python, Express JS)</li>
+            <li>Mobile Development (React Native, Flutter)</li>
+          </ul>
+        </div>}
+        <div className="divider"></div>
+
+        {/* SERVICE NO 04 */}
+        <div className="service1">
+        <div><img className='services_number1' alt='' src={ServicesNumber4} ></img></div>
+        <div className='services1_para'>At Protodev, our content writing services help businesses create engaging and compelling content that resonates with their audience. </div>
+        <div onClick={()=>setShow4(!show4)}><img className='service_arrowbutton' alt='' src={ServicesArrowButton} ></img></div>
+        </div>
+        {show4 && <div className='services1_innerpara'>Our team can deliver high-quality and SEO-friendly content that will help your business succeed. Whether you're in need of website copy, blog posts, articles, or any other form of written content. Our services includes:
+          <ul className='services1_innerul'>
+            <li>Website Copywriting</li>
+            <li>Blog Post Writing</li>
+            <li>Article Writing</li>
+            <li>Product Description Writing</li>
+            <li>Email Copywriting</li>
+            <li>Social Media Copywriting</li>
+          </ul>
+        </div>}
+        <div className="divider"></div>
+
+        {/* SERVICE NO 05 */}
+        <div className="service1">
+        <div><img className='services_number1' alt='' src={ServicesNumber5} ></img></div>
+        <div className='services1_para'>Our digital marketing services at Protodev help businesses reach their target audience and drive growth through various online channels.</div>
+        <div onClick={()=>setShow5(!show5)}><img className='service_arrowbutton' alt='' src={ServicesArrowButton} ></img></div>
+        </div>
+        {show5 && <div className='services1_innerpara'>Our team of experts will work with you to develop a comprehensive digital marketing strategy that includes SEO, PPC, social media, email marketing, and more. We specialize in creating campaigns that are tailored to your business goals and target audience, and we use data-driven insights to optimize and measure the success of your campaign.
+        </div>}
+        <div className="divider"></div>
+
+        {/* SERVICE NO 06 */}
+        <div className="service1">
+        <div><img className='services_number1' alt='' src={ServicesNumber6} ></img></div>
+        <div className='services1_para'>Our social media management services at Protodev help businesses create and execute a comprehensive strategy to increase brand awareness, engage with their audience and drive business growth. </div>
+        <div onClick={()=>setShow6(!show6)}><img className='service_arrowbutton' alt='' src={ServicesArrowButton} ></img></div>
+        </div>
+        {show6 && <div className='services1_innerpara'>Our team of experts will work with you to understand your business and create content that aligns with your brand voice and message. We specialize in managing social media platforms such as Facebook, Instagram, Twitter, LinkedIn, etc. 
+          <ul className='services1_innerul'>
+            <li>Social media account setup and optimization</li>
+            <li>Social media content creation and scheduling</li>
+            <li>Social media advertising and campaign management</li>
+          </ul>
+        </div>}
+      </div>
+    </section>
+  )
+}
+
+export default Services
