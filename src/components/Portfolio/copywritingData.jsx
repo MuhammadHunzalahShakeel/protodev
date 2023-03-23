b610f532242acde119970fbd6c28278b5a9b353a
Sajjad Aziz <azizmsajjad@gmail.com>
2023-03-23 15:50:19 +0500

    portfolio images changed

--- src/components/Portfolio/copywritingData.jsx
+++ src/components/Portfolio/copywritingData.jsx
@@ -1,8 +1,15 @@
+import img1 from "/static/media/PortfolioImage111.svg";
+import pic1 from "/static/media/PortfolioImageBranding1.svg";
+import pic2 from "/static/media/PortfolioImageBranding11.svg";
+import pic3 from "/static/media/PortfolioImageBranding3.svg";
+import pic4 from "/static/media/PortfolioImageBranding33.svg";
+import pic5 from "/static/media/PortfolioImageBranding4.svg";
+import pic6 from "/static/media/PortfolioImageBranding44.svg";
 export const copywritingData = [
     {
-        img:"/static/media/PortfolioImageBranding1.svg",
-        hoverimg:"/static/media/PortfolioImageBranding11.svg",
-        hoverimg2:"/static/media/PortfolioImage111.svg",
+        img: pic1,
+        hoverimg: pic2,
+        hoverimg2: img1,
         text:"FIZZY Branding",
         url:"https://www.behance.net/gallery/164139703/Fizzy-Branding"
     },{
@@ -12,15 +19,15 @@ export const copywritingData = [
         text:"",
         url:""
     },{
-        img:"/static/media/PortfolioImageBranding3.svg",
-        hoverimg:"/static/media/PortfolioImageBranding33.svg",
-        hoverimg2:"/static/media/PortfolioImage111.svg",
+        img: pic3,
+        hoverimg: pic4,
+        hoverimg2: img1,
         text:"DEALSTREE Branding",
         url:"https://www.behance.net/gallery/164139139/Dealstree-Branding"
     },{
-        img:"/static/media/PortfolioImageBranding4.svg",
-        hoverimg:"/static/media/PortfolioImageBranding44.svg",
-        hoverimg2:"/static/media/PortfolioImage111.svg",
+        img: pic5,
+        hoverimg: pic6,
+        hoverimg2: img1,
         text:"BOOMBOX Branding",
         url:"https://www.behance.net/gallery/164140015/Boombox-Branding"
     },{
--- src/components/Portfolio/designData.jsx
+++ src/components/Portfolio/designData.jsx
@@ -1,32 +1,43 @@
+import img1 from "/static/media/PortfolioImage111.svg";
+import pic1 from "/static/media/PortfolioImagedesign1.svg";
+import pic2 from "/static/media/PortfolioImagedesign11.svg";
+import pic3 from "/static/media/PortfolioImagedesign2.svg";
+import pic4 from "/static/media/PortfolioImagedesign22.svg";
+import pic5 from "/static/media/PortfolioImagedesign3.svg";
+import pic6 from "/static/media/PortfolioImagedesign33.svg";
+import pic7 from "/static/media/PortfolioImagedesign4.svg";
+import pic8 from "/static/media/PortfolioImagedesign44.svg";
+import pic9 from "/static/media/PortfolioImagedesign5.svg";
+import pic10 from "/static/media/PortfolioImagedesign55.svg";
 export const designData = [
     {
-        img:"/static/media/PortfolioImagedesign1.svg",
-        hoverimg:"/static/media/PortfolioImagedesign11.svg",
-        hoverimg2:"/static/media/PortfolioImage111.svg",
+        img: pic1,
+        hoverimg: pic2,
+        hoverimg2: img1,
         text:"Food App Design",
         url:"https://www.behance.net/gallery/150337643/Food-Mobile-App"
     },{
-        img:"/static/media/PortfolioImagedesign2.svg",
-        hoverimg:"/static/media/PortfolioImagedesign22.svg",
-        hoverimg2:"/static/media/PortfolioImage222.svg",
+        img: pic3,
+        hoverimg: pic4,
+        hoverimg2: img1,
         text:"NFT Website Design",
         url:"https://www.behance.net/gallery/164166517/Planet-Nft-Landing-Page-Design"
     },{
-        img:"/static/media/PortfolioImagedesign3.svg",
-        hoverimg:"/static/media/PortfolioImagedesign33.svg",
-        hoverimg2:"/static/media/PortfolioImage333.svg",
+        img: pic5,
+        hoverimg: pic6,
+        hoverimg2: img1,
         text:"Trading Website Design",
         url:"https://www.behance.net/gallery/164147031/EZtradesio-Trading-Website-Design"
     },{
-        img:"/static/media/PortfolioImagedesign4.svg",
-        hoverimg:"/static/media/PortfolioImagedesign44.svg",
-        hoverimg2:"/static/media/PortfolioImage444.svg",
+        img: pic7,
+        hoverimg: pic8,
+        hoverimg2: img1,
         text:"Gaming Landing Design",
         url:"https://www.behance.net/gallery/164166877/Thunder-Gaming-Landing-Page-Design"
     },{
-        img:"/static/media/PortfolioImagedesign5.svg",
-        hoverimg:"/static/media/PortfolioImagedesign55.svg",
-        hoverimg2:"/static/media/PortfolioImage555.svg",
+        img: pic9,
+        hoverimg: pic10,
+        hoverimg2: img1,
         text:"Vehicle App Design",
         url:"https://www.behance.net/protodev"
     }
